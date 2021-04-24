6169d0d819d0297c3ee7c66562a6440484a0216c
hamzsait <hamzasait@utexas.edu>
2021-04-24 01:11:01 -0500

    added foreign keys to tables

--- createTables.sql
+++ createTables.sql
@@ -2,23 +2,27 @@ DROP DATABASE IF EXISTS employeeDB;
 CREATE DATABASE employeeDB;
 USE employeeDB;
 
-CREATE TABLE employee(
-    id INTEGER PRIMARY KEY,
-    firstName VARCHAR(30),
-    lastName VARCHAR(30),
-    role_id INTEGER,
-    manager_id INTEGER
+CREATE TABLE departments(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(30)
 );
 
 CREATE TABLE roles(
-    id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     title VARCHAR(30),
     salary DECIMAL(10,2),
-    department_id INTEGER
+    department_id INTEGER,
+    FOREIGN KEY (department_id) REFERENCES departments(id)
 );
 
-CREATE TABLE department(
-    id INTEGER PRIMARY KEY,
-    name VARCHAR(30)
+CREATE TABLE employees(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    firstName VARCHAR(30),
+    lastName VARCHAR(30),
+    role_id INTEGER,
+    FOREIGN KEY (role_id) REFERENCES roles(id),
+    manager_id INTEGER
 );
+
+
 
