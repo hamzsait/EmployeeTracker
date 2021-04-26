327a1cbaa5dba6249b06d7ea832615d770f44148
hamzsait <hamzasait@utexas.edu>
2021-04-25 19:23:17 -0500

    added data injection file

---
+++ injectData.sql
@@ -0,0 +1,11 @@
+USE employeeDB;
+
+INSERT INTO roles (title,salary,department_id)
+VALUES ("Intern",20000,1);
+
+INSERT INTO roles (title,salary,department_id)
+VALUES ("Engineer",60000,1);
+
+INSERT INTO roles (title,salary,department_id)
+VALUES ("Developer",40000,1);
+
